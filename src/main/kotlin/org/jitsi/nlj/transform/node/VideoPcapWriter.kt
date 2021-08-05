cb799927c368391f611b25d3338d0307a9012e07
tu-nv <vantu.bkhn@gmail.com>
2021-08-05 02:01:37 +0000

    individual_rec: handle the case where VP8 and VP9 are switched dynamically due to firefox and safari users

--- src/main/kotlin/org/jitsi/nlj/transform/node/VideoPcapWriter.kt
+++ src/main/kotlin/org/jitsi/nlj/transform/node/VideoPcapWriter.kt
@@ -17,14 +17,13 @@ package org.jitsi.nlj.transform.node
 
 import java.net.Inet4Address
 import org.jitsi.nlj.PacketInfo
-import org.jitsi.nlj.codec.vp8.Vp8Utils
-import org.jitsi.nlj.rtp.VideoRtpPacket
 import org.jitsi.nlj.rtp.codec.vp8.Vp8Packet
+import org.jitsi.nlj.rtp.codec.vp9.Vp9Packet
 import org.jitsi.utils.logging.DiagnosticContext
-import org.jitsi.utils.logging2.cinfo
 import org.jitsi.utils.logging2.createChildLogger
 import org.jitsi.utils.logging2.Logger
-import org.jitsi_modified.impl.neomedia.codec.video.vp8.DePacketizer
+import org.pcap4j.core.PcapDumper
+import org.pcap4j.core.PcapHandle
 import org.pcap4j.core.Pcaps
 import org.pcap4j.packet.EthernetPacket
 import org.pcap4j.packet.IpV4Packet
@@ -51,28 +50,47 @@ class VideoPcapWriter(
     private val confName = diagnosticContext["conf_name"].toString().split("@")[0]
     private val confSite = diagnosticContext["conf_name"].toString().split("@")[1].split(".")[1]
     private val confId = diagnosticContext["conf_id"]
-    private val filePath = "$videoPcapDir/${confSite}_${confName}__${confId}__$endPointID.pcap"
-    private val filePathTmp = "$videoPcapDir/$endPointID.pcap.tmp"
-
-    private val lazyHandle = lazy {
-        Pcaps.openDead(DataLinkType.EN10MB, 65536)
-    }
-    private val handle by lazyHandle
-    private val lazyWriter = lazy {
-        logger.cinfo { "Pcap writer writing for endpoint $endPointID" }
-        handle.dumpOpen(filePathTmp)
-    }
-    private val writer by lazyWriter
-
-    private var videoSsrcToCap: Long? = null
-    private val vp8PayloadType = 100
-    private val resToCap = 180
+    private val videoPcapDir = videoPcapDir
+    private val endPointID = endPointID
+    private var handle: PcapHandle? = null
+    private var writer: PcapDumper? = null
+    private var filePath: String? = null
 
     companion object {
         private val localhost = Inet4Address.getByName("127.0.0.1") as Inet4Address
     }
 
+    private var vp8SsrcToCap: Long? = null
+    private var curPacketType: String? = null
+    private var pcapFileIndex = 0
+
     override fun observe(packetInfo: PacketInfo) {
+        // for VP8, simulcast is used, therefore we do not capture VP8 packets that do not have desired resolution
+        if (packetInfo.packet is Vp8Packet) {
+            if (curPacketType != "VP8") {
+                closeOldWriter()
+                createNewWriter("VP8")
+            }
+
+            val vp8Packet = packetInfo.packetAs<Vp8Packet>()
+            if (vp8SsrcToCap == null && vp8Packet.height == 180)
+                vp8SsrcToCap = vp8Packet.ssrc
+
+            if (vp8Packet.ssrc != vp8SsrcToCap)
+                return
+        }
+
+        if (packetInfo.packet is Vp9Packet) {
+            if (curPacketType != "VP9") {
+                closeOldWriter()
+                createNewWriter("VP9")
+            }
+        }
+
+        // if not VP8 or VP9, then do not capture
+        if (!(packetInfo.packet is Vp8Packet || packetInfo.packet is Vp9Packet))
+            return
+
         val udpPayload = UnknownPacket.Builder()
         // We can't pass offset/limit values to udpPayload.rawData, so we need to create an array that contains
         // only exactly what we want to write
@@ -106,23 +124,36 @@ class VideoPcapWriter(
                 .payloadBuilder(ipPacket)
                 .build()
 
-        writer.dump(eth)
+        writer!!.dump(eth)
     }
 
     override fun trace(f: () -> Unit) = f.invoke()
 
-    fun close() {
-        if (lazyWriter.isInitialized() && writer.isOpen) {
-            writer.close()
-        }
+    private fun closeOldWriter() {
+        if (writer != null && writer!!.isOpen)
+            writer!!.close()
 
-        if (lazyHandle.isInitialized() && handle.isOpen) {
-            handle.close()
-            renameFinalPcap(filePathTmp, filePath)
+        if (handle !=null && handle!!.isOpen) {
+            handle!!.close()
+            renamePcap(filePath!!, "${filePath}.pcap")
         }
     }
 
-    private fun renameFinalPcap(oldFilePath: String, newFilePath: String): Boolean {
+    fun close() {
+        closeOldWriter()
+        // user finish text to indicate finish recording for this participant ID
+        renamePcap("$filePath.pcap", "${filePath}finish.pcap")
+    }
+
+    private fun createNewWriter(newPacketType: String) {
+        curPacketType = newPacketType
+        filePath = "$videoPcapDir/${confSite}_${confName}__${confId}__${endPointID}__${pcapFileIndex}__${newPacketType}__"
+        handle = Pcaps.openDead(DataLinkType.EN10MB, 65536)
+        writer = handle!!.dumpOpen(filePath)
+        pcapFileIndex += 1
+    }
+
+    private fun renamePcap(oldFilePath: String, newFilePath: String): Boolean {
         val oldFile = File(oldFilePath)
         val newFile =  File(newFilePath)
         return oldFile.renameTo(newFile)
